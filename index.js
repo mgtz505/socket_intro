041229e1dc9d3fa69a55224ebea095b3b1ab5923
mgtz505 <gutierrez.marcusc@gmail.com>
2021-04-07 00:39:59 -0400

    Completed my introduction to Socket.io

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+  });
+});
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
